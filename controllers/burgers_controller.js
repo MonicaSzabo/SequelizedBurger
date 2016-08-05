5c4d99d292bab4ef28dafd0cec58f3845ff3e873
Monica Szabo <monicaszb@gmail.com>
2016-08-04 22:16:35 -0500

    routes working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,36 +1,34 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../models');
+var models = require('../models');
 
 router.get('/', function (req, res) {
 	res.redirect('/burgers');
 });
 
 router.get('/burgers', function (req, res) {
-	db.burger.findAll().then(function (data) {
+	models.burger.findAll().then(function (data) {
 		var hbsObject = { burgers: data };
 		res.render('index', hbsObject);
 	});
 });
 
 router.post('/burgers/create', function (req, res) {
-	db.burger.findOne(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], function () {
+	models.burger.create({burger_name: req.body.burger_name, devoured: req.body.devoured}).then(function() {
 		res.redirect('/burgers');
 	});
 });
 
 router.put('/burgers/update/:id', function (req, res) {
-	var condition = 'id = ' + req.params.id;
 
-	db.burger.update({ devoured: req.body.devoured }, condition, function () {
+	models.burger.update({ devoured: req.body.devoured }, {where: {id:req.params.id}}).then(function () {
 		res.redirect('/burgers');
 	});
 });
 
 router.delete('/burgers/delete/:id', function (req, res) {
-	var condition = 'id = ' + req.params.id;
 
-	db.burger.destroy('burgers', condition, function() {
+	models.burger.destroy({where: {id:req.params.id}}).then(function() {
 		res.redirect('/burgers');
 	});
 });
